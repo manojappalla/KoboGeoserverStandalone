11d387722fe33dd6b8f3dced6b32fea2666aa4d6
manojappalla <manojappalla@outlook.com>
2022-08-19 17:46:09 +0530

    Written code to add the last used form id and to update the fields of the newly created shapefile.

--- DownloadToShpAndPublishOdk.py
+++ DownloadToShpAndPublishOdk.py
@@ -30,7 +30,9 @@ username_odk = odk['Odk Credentials']['username']
 password_odk = odk['Odk Credentials']['password']
 last_submission_odk = odk['Odk Credentials']['last submission']
 form_feature_count_odk = 0
-last_project_used_odk = odk['Projects ODK']['last used']
+last_project_used_odk = odk['Odk Credentials']['last used project']
+last_form_used_odk = odk[last_project_used_odk]['last selected form']
+last_selected_form_id = odk[last_project_used_odk][last_form_used_odk]
 
 # ODK-GEOSERVER VARIABLES
 shp_workspace_name_odk = config['Shapefile Workspace Store']['workspace_name_odk']
@@ -141,6 +143,148 @@ class ImportOdk():
                 new_dict[key] = val
         return new_dict
 
+    def importData(self, layer, selectedForm, doImportData=True):
+        """Imports user selected form from server """
+
+        # from central
+        user = username_odk
+        project_id = self.project_id
+        password = password_odk
+        c_url = url_odk
+        if not c_url:
+            print("Enter url in OdkAuth.ini file")
+            return None, None
+        data = {'email': user, 'password': password}
+        headers = {"Content-Type": "application/json"}
+        requests.packages.urllib3.disable_warnings()
+        selectedFormName = ""
+        form_response = requests.get(c_url + "v1/projects/" + str(project_id) + "/forms/",
+                                     headers={"Authorization": "Bearer " + self.usertoken})
+        for form in form_response.json():
+            if form["enketoOnceId"] == selectedForm:
+                selectedFormName = form["name"]
+                self.form_name = selectedFormName
+        try:
+            response = requests.get(c_url + 'v1/projects/' + str(project_id) + '/forms/' + selectedFormName + '.xml',
+                                    headers={"Authorization": "Bearer " + self.usertoken})
+        except:
+            print("Invalid url,username or password")
+            return
+        if response.status_code == 200:
+            xml = response.content
+            print(xml)
+            self.layer_name, self.version, self.geoField, self.fields = self.updateLayerXML(layer, xml)
+            # layer.setName(self.layer_name)
+            # self.collectData(layer, selectedForm, doImportData, self.layer_name, self.version, self.geoField)
+        else:
+            print("not able to connect to server")
+
+    def updateLayerXML(self,layer,xml):
+
+        """
+        In this function we are trying to extract the following:
+
+        1. tite of the selected form
+        2. instance of the selected form
+        3. version of the selected form
+        4. field names and field types from the <bind> tag in the XML response
+        5. qgstype, config of the attribute type using qtype function which is user defined is also extracted
+        6. extract the geoField from all the fields and set the isHidden of all the fields as false.
+        7. Next we pass on layer, fieldNames, qgstypes, configs of all the fields to updateFields function
+
+        :param layer:
+        :param xml:
+        :return:
+        layer_name,version,geoField,fields
+        """
+
+        geoField=''
+        ns='{http://www.w3.org/2002/xforms}'
+        nsh='{http://www.w3.org/1999/xhtml}'
+        root= ET.fromstring(xml)
+        #key= root[0][1][0][0].attrib['id']
+        layer_name=root[0].find(nsh+'title').text
+        instance=root[0][1].find(ns+'instance')
+        fields={}
+        #topElement=root[0][1][0][0].tag.split('}')[1]
+        try:
+            version=instance[0].attrib['version']
+        except:
+            version='null'
+#        print('form name is '+ layer_name)
+#        print (root[0][1].findall(ns+'bind'))
+        for bind in root[0][1].findall(ns+'bind'):
+            attrib=bind.attrib
+            fieldName= attrib['nodeset'].split('/')[-1]
+            try:
+                fieldType=attrib['type']
+            except:
+                continue
+            fields[fieldName]=fieldType
+            # print('attrib type is',attrib['type'])
+            qgstype,config = qtype(attrib['type'])
+#            print ('first attribute'+ fieldName)
+            inputs=root[1].findall('.//*[@ref]')
+            if fieldType[:3]!='geo':
+                #print('creating new field:'+ fieldName)
+                isHidden= True
+                if fieldName=='instanceID':
+                    fieldName='ODKUUID'
+                    fields[fieldName]=fieldType
+                    isHidden= False
+                for input in inputs:
+                    if fieldName == input.attrib['ref'].split('/')[-1]:
+                        isHidden= False
+                        break
+                if isHidden:
+                    # print('Reached Hidden')
+                    config['type']='Hidden'
+            else:
+                geoField=fieldName
+                # print('geometry field is =',fieldName)
+                continue
+            self.updateFields(layer,fieldName,qgstype,config)
+        return layer_name,version,geoField,fields
+
+    def updateFields(self, layer, text='ODKUUID', q_type=QVariant.String, config={}):
+
+        """
+
+        This function writes the attribute names and types along with the config to the shapefile.
+        :param layer:
+        :param text:
+        :param q_type:
+        :param config:
+        :return:
+        """
+
+        flag = True
+        for field in layer.fields():
+
+            if field.name()[:10] == text[:10]:
+                flag = False
+                # print("not writing fields")
+        if flag:
+            uuidField = QgsField(text, q_type)
+            if q_type == QVariant.String:
+                uuidField.setLength(300)
+            layer.dataProvider().addAttributes([uuidField])
+            layer.updateFields()
+        fId = layer.dataProvider().fieldNameIndex(text)
+        try:
+            if config['type'] == 'Hidden':
+                # print('setting hidden widget')
+                layer.setEditorWidgetSetup(fId, QgsEditorWidgetSetup("Hidden", config))
+                return
+        except Exception as e:
+            # print(e)
+            pass
+        if config == {}:
+            return
+        # print('now setting external resource widget')
+        layer.setEditorWidgetSetup(fId, QgsEditorWidgetSetup("ExternalResource", config))
+        # print("done")
+
 """
 *************************************************************************************************
                                KOBO ---> TESTING getFormList
@@ -152,3 +296,11 @@ try:
     print(data_kobo.getFormList())
 except:
     print("Invalid credentials entered")
+
+"""
+*************************************************************************************************
+                               KOBO ---> TESTING importData
+*************************************************************************************************
+"""
+layer = QgsVectorLayer('/Users/saimanojappalla/Desktop/OdkShpTest/test.shp', "new", "ogr")
+data_kobo.importData(layer, last_selected_form_id)
